173ae2b84c57502a9d7bafa77dd375bd7f43a3a6
imxamanda <amandavieiraperes.12@gmail.com>
2023-09-10 19:43:18 -0300

    image

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [customInterval, setCustomInterval] = useState();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+          style={styles.image}
+          source={require('./assets/cronometroimg.png')}
+        />
     </View>
   );
 }
@@ -13,8 +21,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#b8a6d8',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    height: 250,
+    width: 250
+
+  }
 });
