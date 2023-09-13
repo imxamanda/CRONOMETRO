ff6eb2a87cd819f733e33ab87830918480d3fa36
imxamanda <amandavieiraperes.12@gmail.com>
2023-09-13 13:56:16 -0300

    13/09

--- App.js
+++ App.js
@@ -1,27 +1,124 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+let timer = null;
+let ss = 0;
+let mm = 0;
+let hh = 0;
 
 export default function App() {
 
-  const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [customInterval, setCustomInterval] = useState();
+  const [display, setDisplay] = useState('00:00:00');
+  const [botaoText, setBotaoText] = useState('Iniciar');
+  const [lastTime, setLastTime] = useState(null);
+
+  function start() {
+    if (timer !== null) {
+      clearInterval(timer);
+      timer = null;
+      setBotaoText('Iniciar');
+    } else {
+      timer = setInterval(() => {
+        ss++;
+
+        if (ss === 60) {
+          ss = 0;
+          mm++;
+        }
+        if (mm === 60) {
+          mm = 0;
+          hh++;
+        }
+
+        let format = (hh < 10 ? '0' : '') + hh
+          + ':' + (mm < 10 ? '0' : '') + mm
+          + ':' + (ss < 10 ? '0' : '') + ss;
+
+        setDisplay(format);
+      }, 10);
+
+      setBotaoText('Parar');
+    }
+  }
+
+  function clear() {
+    if (timer !== null) {
+      clearInterval(timer);
+      timer = null;
+      setBotaoText('Iniciar')
+    }
+
+    if (display !== "00:00:00") {
+      setLastTime([display, ...lastTime]);
+    }
+
+    setDisplay("00:00:00");
+    ss = 0;
+    mm = 0;
+    hh = 0;
+
+
+    setLastTime(display);
+
+    setDisplay('00:00:00');
+    setBotaoText('Iniciar');
+    
+  }
+
+
 
   return (
     <View style={styles.container}>
-      <Image
+      <View>
+        <Image
           style={styles.image}
           source={require('./assets/cronometroimg.png')}
         />
+        <Text style={styles.displayText}>{display}</Text>
+      </View>
+
+      <View style={styles.buttons}>
+        <TouchableOpacity 
+        style={[styles.button]}
+         onPress={start}> 
+        <Text style={styles.buttonText}>{botaoText}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+        style={[styles.button]} 
+        onPress={clear}>
+        <Text style={styles.buttonText}>Limpar</Text>
+        </TouchableOpacity>
+
+       
+      </View>
+
+      {/* <View style={styles.last}>
+        <Text style={styles.last}>Ultimo tempo salvo:</Text>
+        {timesSave.map((tempo, index) => (
+          <Text key={index} style={[styles.historic, styles.historicItem]}>{`Tempo ${index + 1}: ${tempo}`}</Text>
+        ))}
+      </View> */}
+
+      <View style={styles.last}>
+        <Text style={styles.last}> {lastTime ? 'Ultimo tempo:' + lastTime: ''}
+
+        </Text>
+
+      </View>
+
+
+
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#b8a6d8',
+    backgroundColor: '',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -29,5 +126,47 @@ const styles = StyleSheet.create({
     height: 250,
     width: 250
 
+  },
+  displayText: {
+      fontSize: 30,
+      textAlign: 'center',
+  },
+  buttons:{
+    flexDirection: 'row',
+    gap: 20,
+    marginTop: 45,
+  },
+  button:{
+    backgroundColor: '#b8a6d8',
+    borderRadius: 100,
+    paddingTop: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    width: 120,
+    marginBottom: 20,
+    borderColor: '#402c63',
+    borderWidth: 1,
+  },
+  buttonText:{
+    
+  },
+ 
+  last:{
+    fontSize: 16,
+    marginBottom: 10,
+  },
+
+  historic:{
+    fontSize: 25,
+    marginTop: 2,
+    paddingHorizontal: 25,
+    textAlign:'center',
+    color:'white',
+  },
+  historicItem: {
+      fontSize: 20,
+      marginBottom: 5,
   }
+
 });
+
